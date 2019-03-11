7eada415a6ea979a05b4f4ab28b2ac4362fb569a
Kipras Melnikovas <kipras@kipras.org>
2019-03-11 21:55:35 +0200

    Configure stuff with docker!
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- models/Item.js
+++ models/Item.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+	name: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
-module.exports = Item = mongoose.model('item', ItemSchema);
+module.exports = Item = mongoose.model("item", ItemSchema);
